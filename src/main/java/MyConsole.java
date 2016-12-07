319bdd313d0bc1bad5852e4caeb85d76d4ffca5b
Yael_Zaritsky <yaelz@wix.com>
2016-12-07 15:59:48 +0200

    Added sum numbers feature

--- src/main/java/MyConsole.java
+++ src/main/java/MyConsole.java
@@ -8,8 +8,31 @@ public class MyConsole {
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
-            System.out.print("> ");
-            System.out.println(inputReader.readLine());
+            System.out.println("Please enter one of the following actions: sum / copycat");
+
+            String input = inputReader.readLine();
+            if ("copycat".equals(input)) {
+                System.out.print(">");
+                System.out.println(inputReader.readLine());
+            } else if ("sum".equals(input)) {
+                System.out.println("Enter numbers to sum, separated by space. For example: '233 67 8 456086'");
+                String nToSIn = inputReader.readLine();
+                String[] nToS = nToSIn.split(" ");
+
+                String str = "Sum of ";
+
+                int sum = 0;
+
+                for (int i = 0; i < nToS.length; i++) {
+                    str += nToS[i] + ", ";
+                    sum += Integer.parseInt(nToS[0]);
+                }
+                str = str.substring(0, str.length() - 2);
+
+                str += "is " + sum;
+
+                System.out.println(str);
+            }
         }
     }
 
